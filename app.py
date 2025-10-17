569d713f94653faea8da27e017a7152208329b6f
Jaime Rodriguez <jaimesrdz17@gmail.com>
2025-10-17 17:17:21 -0500

    Removed Twilio, Added API Key

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ import base64
 import re
 import requests
 
-from twilio.rest import Client  
 
 app = Flask(__name__)
 
@@ -24,14 +23,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-openai.api_key = "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"
+openai.api_key = "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"
 app.secret_key = "supersecretkey"
-
-# Twilio credentials 
-account_sid = "ACe9e96fffa3a05bff45cd3c9749d73427"
-auth_token = "b45a3727d3e2b7ea6fb54811b6854e6e"
-twilio_phone_number = "+18557270309"  # Your Twilio phone number
-user_phone_number = "+17374020057"    # The user's phone number for testing
 
 # Open and read txt file, create variable for the contents
 with open("topic_prompts/directive.txt", "r") as file:
@@ -51,22 +44,6 @@ def determine_fire_risk_level(fire_alert_context):
         return "moderate fire hazard"
     else:
         return "no fire hazard"
-
-# Function to send SMS via Twilio
-def send_sms_via_twilio(to_number, from_number, body):
-    url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json'
-    data = {
-        'To': to_number,
-        'From': from_number,
-        'Body': body
-    }
-    try:
-        response = requests.post(url, data=data, auth=(account_sid, auth_token))
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        app.logger.info(f"SMS sent: {response.json()['sid']}")
-    except requests.exceptions.RequestException as e:
-        app.logger.error(f"An error occurred while sending SMS: {e}")
-
 
 @app.route("/")
 def home():
@@ -126,16 +103,6 @@ def process_image():
         # Determine the fire risk level
         risk_level = determine_fire_risk_level(fire_alert_context)
 
-        # Decide whether to send SMS
-        if risk_level in ["imminent fire emergency", "high-risk fire hazard", "moderate fire hazard"]:
-            # Send SMS
-            send_sms_via_twilio(
-                to_number=user_phone_number,
-                from_number=twilio_phone_number,
-                body=fire_alert_context
-            )
-
-        return jsonify({"context": fire_alert_context})
     except Exception as e:
         app.logger.error(f"An error occurred: {e}")
         return jsonify({"error": str(e)}), 500
